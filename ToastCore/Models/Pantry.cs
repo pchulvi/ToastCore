76356a345d5e12c18b2cdec4f28fd60bc8527f77
pchulvi <pchulvi@users.noreply.github.com>
2018-01-23 11:51:02 +0100

    Añadido sln

---
+++ ToastCore/Models/Pantry.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToastCore.Models
+{
+    public class Pantry
+    {
+        public int Id { get; set; }
+
+        public int NumberOfBreads { get; set; }
+
+        public PantryStatus Status { get; set; }
+
+
+       
+    }
+}
---
+++ ToastCore/Models/Status.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToastCore.Models
+{
+    public enum Status
+    {
+        Off = 0,
+        On = 1
+    }
+}
