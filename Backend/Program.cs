de90892fc770d4721ea431bdce0c226be077664b
jajAmaris <joseantonio.jurado@amaris.com>
2018-01-30 13:21:57 +0100

    Program.cs de Backend, añade un puerto por defecto, el 5010 para nginx

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System.IO;
+using Backend.Configuration;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
 
 namespace Backend
 {
@@ -7,8 +10,22 @@ namespace Backend
     {
         public static void Main(string[] args)
         {
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false)
+                .Build();
+
+            var configHosting = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("hosting.json", optional: true)
+                .Build();
+
+            ApplicationConfigurationsSettings settings = new ApplicationConfigurationsSettings();
+
+            config.GetSection("ApplicationConfigurations").Bind(settings);
+
             var host = new WebHostBuilder()
                 .UseKestrel()
+                .UseUrls("http://*:5010") /*Esto es para que cuando utilicemos kestrel en linux nos de la dirección http://0.0.0.0:5010 */
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
@@ -17,5 +34,12 @@ namespace Backend
 
             host.Run();
         }
+
+        public class ApplicationConfigurationsSettings
+        {
+            public string ApplicationPort { get; set; }
+        }
+
+        public IOptions<ApplicationConfigurations> OptionsApplicationConfiguration { get; }
     }
 }
